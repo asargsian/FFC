1f798e75f327d4802d6efb65c3f1a2a18f4b0656
Ann <anahit_sargsian@outlook.com>
2018-03-03 04:43:23 +0400

    Revision 2

--- ffc.py
+++ ffc.py
@@ -1,14 +1,19 @@
-from __future__ import print_function
 import pandas as pd
 import numpy as np
-from fancyimpute import KNN
 from sklearn import preprocessing
+import sklearn.ensemble
+from fancyimpute import KNN
 import lime
 import lime.lime_tabular
 import pylab as pl
 from sklearn.tree import export_graphviz as eg
 from copy import deepcopy
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.cluster import KMeans
+import statsmodels.api as sm
+from itertools import chain
+
 
 background = pd.read_csv("background.csv")
 
@@ -84,17 +89,22 @@ background = background.dropna(axis=1, thresh = 400)
 #Missing value imputation with KNN
 background_filled_knn = KNN(k=100).complete(background)
 background_filled_knn = pd.DataFrame(background_filled_knn, columns=[list(background)])
+background_filled_knn.to_csv('background_filled_knn.csv')
 
-train = pd.read_csv('/Users/ann/Desktop/FF/FFChallenge_v2/train.csv');
+train = pd.read_csv('train.csv');
 train = train[['challengeID', 'gpa']]
 train = train.dropna()
-training = pd.concat([background_filled_knn.set_index('challengeID'),train.set_index('challengeID')], axis=1, join='inner');
+training = pd.concat([background_filled_knn, train],axis=1, join='inner');
+
+#training.to_csv('concated_train_background.csv')
+
+
 
 
 #getting features from rlasso with tuning
 feature_set = []
 from sklearn.linear_model import RandomizedLasso
-params = [0.0000004, 0.00000004]
+params = [0.004, 0.0000004]
 predictors = training.iloc[:, 1:len(list(training))-1]
 targets = training['gpa']
 pred_train, pred_test, tar_train, tar_test  = train_test_split(predictors, targets, test_size=.3)
@@ -119,41 +129,73 @@ result = pred_train.iloc[:, indices[:500]]
 feature_set.append(list(result))
 
 #intersect three sets of features obtained above
-intrs = list(set(feature_set[0]).intersection(*feature_set[1:]))
+features = list(set(feature_set[0]).intersection(*feature_set[1:]))
+
+
+if len(features) <=30:
+    print "WARNING: the selected set of features most probably is not a proper one, and may result in low prediction accuracy. \n Please stop the experiment and rerun. If this message persists, we recommend omitting the feature selection step and instead using the features in line 138."
+
+recommended = ['m1e3b4', 'k5a2f', 'p5i23', 'hv4l18', 'hv4l17', 'm4i0l', 'f4h2a_2',
+               'p5q3u', 'm2g6d1', 'm2f2b3', 'o5c6', 'm5e1n', 'm1i1', 'm1i3',
+               'm5f24', 'p5q3g', 'm5f23c', 'o5c9', 'f5g35', 'm1e3b5', 'k5g2m',
+               'k5g2h', 'hv3m2c', 'f2g1a', 't5c13b', 'p5i35', 't5b3e', 'p5m1',
+               't5c8b', 'm5b30', 'm4a14', 'm4a15', 'hv5_ppvtpr', 'p5l15a_5',
+               'hv4p7c', 'hv3m23', 'f3h6a', 'm1d2c', 'm5e8_5', 'hv3c5', 't5e19e',
+               'f5e1h', 'p5l18', 'cf1edu', 'hv3r9', 'm2h14a4', 'm1e1c1', 't5b1w',
+               'm3k14b4', 't5b1u', 'f5k17g', 'hv5_wj10pr', 'f4i5a', 'm3i18',
+               'm3h6', 'f1b8', 'k5g2d', 'f2d7g', 'hv5_ppvtss', 'm1b4g', 'k5a1a',
+               'hv3m21', 'p5l5', 'f3b3', 'p5q3bw', 'f5k2a', 'hv3mmis_wt',
+               'hv5_wj10ae', 'f1b20']
 
 #using Random Forest for prediction
 from sklearn.ensemble import RandomForestRegressor
-i=200
-while True:
-    predictors = training[intrs]
+
+max_score = 0
+
+scores = []
+for i in range(200,400,5):
+    predictors = training[features]
     targets = training['gpa']
     pred_train, pred_test, tar_train, tar_test  = train_test_split(predictors, targets, test_size=.2)
     reg=RandomForestRegressor(n_estimators=i, max_depth=None, min_samples_split=3)
     reg=reg.fit(pred_train,tar_train)
     predictions = reg.predict(pred_test)
     score = reg.score(pred_test, tar_test)
-    i+=1
-    if score >=0.3:
-        all_pred = reg.predict(background[intrs])
-        pd.DataFrame(all_pred).to_csv('intersection_pred_abv_score'+str(score)[:10]+'.csv')
-    print(i, score)
+    scores.append((i, score))
+    print (i, score)
+    if score >= max_score:
+        all_pred = reg.predict(predictors[features])
+        pd.DataFrame(all_pred).to_csv('final_check_intersection_pred_abv_score'+str(score)[:10]+'.csv')
 
+
+
+#reading names of features
+import re
+featdict=dict(re.compile('---\r\n(\S+).+?(\S.+?)\r\n').findall(open("codebook_FFChallenge.txt").read()))
+names=[featdict[tmp] for tmp in features]
 
 #Decision tree
 
 gpas = np.arange(1, 4.25, 0.25)
 for i in gpas:
     if i < 2.75:
-        training['gpa'][training['gpa']==i] = 'Low'
+        training.loc[training['gpa']==i, 'gpa'] = 'Low'
     elif i < 3.25 and i >= 2.75:
-        training['gpa'][training['gpa']==i] = 'Mid'
+        training.loc[training['gpa']==i, 'gpa'] = 'Mid'
     elif i <= 4 and i >= 3.15:
-        training['gpa'][training['gpa']==i] = 'Top'
-training['gpa'] = training['gpa'].astype('category')
+        training.loc[training['gpa']==i, 'gpa'] = 'Top'
+training.gpa = training.gpa.astype('category')
+
+test = training.values
+pred = test[:, :-2]
+target = test[:, -1]
+
 
 train,test,labels_train,labels_test=pred,pred,target,target
 train_hl=train[(labels_train=='Top') | (labels_train=='Low')]
 labels_train_hl=labels_train[(labels_train=='Top') | (labels_train=='Low')]
+
+
 
 
 dt=sklearn.tree.DecisionTreeClassifier(max_depth=5)
@@ -163,7 +205,9 @@ len(pl.find(training.gpa=='Low'))
 
 pd.value_counts(labels_train_hl)
 
+
 # Shortened names of important features
+
 
 names_mod=deepcopy(names)
 names_mod[7]="Disobedient at home"
@@ -172,10 +216,8 @@ names_mod[47]='Child attends to instructions'
 names_mod[24]="Child's science and social studies"
 [tmp for tmp in range(len(names)) if 'C13B' in names[tmp]]
 
-
 eg(dt,out_file='l1_tree.dot',class_names=('Low','Top'),rounded=True,feature_names=names_mod,max_depth=2,filled=True,impurity=False)
 eg(dt,out_file='l2_tree.dot',class_names=('Low','Top'),rounded=True,feature_names=names_mod,max_depth=3,filled=True,impurity=False)
-
 
 
 #LIME
@@ -200,11 +242,14 @@ pickle.dump((train_hl,labels_train_hl,names),open("training_featurematrix_andfea
 pickle.dump(exps,open("explanations.pkl",'w+'))
 pickle.dump(exps_1,open("explanations_label2.pkl",'w+'))
 
+
+
 #K-Means clustering
 
 # Converts each list into a fixed feature vector for clustering
 
 # Use convention from original (label=0) file
+
 exps0 = pickle.load(open("./explanations.pkl"))
 featlist = list(set(chain(*[[tmp[0] for tmp in e.as_list(label=0)] for e in exps0])))
 featinds = {featlist[ind]: ind for ind in range(len(featlist))}
@@ -224,7 +269,7 @@ expmat=np.array([CreateVec(tmp) for tmp in inlists])
 
 km=KMeans(n_clusters=5,random_state=100)
 km.fit(expmat)
-km.labels_.to_csv('km_labels.csv')
+np.savetxt('km_labels.csv',km.labels_, delimiter=',', header='0')
 
 # Presenting KMeans results
 
@@ -253,6 +298,8 @@ pl.ylabel('Logistic Regression Coefficient')
 pl.show()
 pl.savefig('LR_c.01.png')
 
+
+
 #logistic regression
 # utility functions
 
@@ -264,9 +311,9 @@ def PlotCoefs(coefs,xl='Feature',yl='Logistic Regression Coefficient'):
     pl.ylabel(yl)
 
 #getting clusters along with labels
-labels = pd.read_csv('km_labels.csv')['0'].tolist()
+labels = pd.read_csv('km_labels.csv')['# 0'].values.tolist()
 
-train = pk.load(open('training_datamatrix_andfeaturenames.pkl'))
+train = pickle.load(open('training_featurematrix_andfeaturenames.pkl'))
 
 data_all = pd.DataFrame(train[0], columns=train[2])
 
@@ -316,11 +363,13 @@ for tmp in [[0, 2], [1], [3], [4]]:
     # Getting significant, sorted coeffs
     betas = np.abs(coefs.params[coefs.pvalues < .05])
     inds = np.argsort(betas)[::-1]
-    sigcoefs = data_all.columns[coefs.pvalues < .05][inds]
+    sigcoefs = data[train_cols].columns[coefs.pvalues < .05][inds]
 
     # Generating/populating output
     significants.append(sigcoefs)
     # print coefs
 
 # Printing
-print '\n'.join(['* ' + ', '.join(tmp) for tmp in significants])
+print ('\n'.join(['* ' + ', '.join(tmp) for tmp in significants]))
+
+
